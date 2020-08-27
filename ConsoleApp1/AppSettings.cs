b4ae5c3058adffec09d949493f8ea83b4d6a8861
Tim Kopp <tim.kopp@cyber-balance.com>
2020-08-27 09:01:00 -0400

    updated parser

--- ConsoleApp1/AppSettings.cs
+++ ConsoleApp1/AppSettings.cs
@@ -12,10 +12,10 @@ namespace Searcher
             public static string BasePath = ConfigurationManager.AppSettings["BasePath"].ToString(); 
             public static string SourceDir = ConfigurationManager.AppSettings["SourceDir"].ToString();
             public static string DestDir = ConfigurationManager.AppSettings["DestDir"].ToString();
-            public static string ExcludeList = ConfigurationManager.AppSettings["ExcludeList"].ToString();
+            public static string PathExclusions = ConfigurationManager.AppSettings["PathExclusions"].ToString();
 
-            public static string DirCustomControls = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\CustomControls\TIC_*";
+            public static string DirCustomControls = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\CustomControls\";
             public static string DirCode = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\";
-            public static string DirDB = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\database\functions\";
+            public static string DirDB = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\database\";
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,30 +16,41 @@ namespace Searcher
     {
         static void Main(string[] args)
         {
-
-            Cache.Write("");
-            ParseBuilder <AppSettingsParser> PB = new ParseBuilder<AppSettingsParser>();
-            PB.Init()  
-                .Compilers(new List<ICompiler>() {
-                    new LineExtractor("ngModel", 1),
-                    new RegexCompile(@"\[  ~~~~~ *\]", "")
-                })
-                .ParseTo( new SOM.IO.CacheEditor() );
-
-            Cache.Write(
-                Cache.Read().RemoveEmptyLines()
-            );
-            Cache.CacheEdit();
+            //new LineExtractor("COLUMN", 5)
+            ParseBuilder<DBParser> PB = new ParseBuilder<DBParser>();
+            PB.AddCompiler(new LineExtractor("INSERT INTO AuditLog", 2))
+              .ParseTo(  new CacheWriter(), (r)=>(r)  ); 
+            Cache.CacheEdit(); 
         }
-
+        //       
+        public class DBParser : BaseParser
+        {
+            public DBParser()
+            {
+                Path =AppSettings.DirDB; 
+                PathExclusions.AddRange(AppSettings.PathExclusions.Split(new char[] { ',' }));
+                PathExclusions.AddRange("\\Triggers,\\Utils,\\InProgress".Split(new char[] { ',' }));
+                //this.Compilers.Add(new RegexCompile("--.*", "")); 
+            }
+        }
+        public class CodeBaseParser : BaseParser
+        {
+            public CodeBaseParser()
+            {
+                Path = AppSettings.DirCode + ""; 
+                PathExclusions.AddRange(AppSettings.PathExclusions.Split(new char[] { ',' }));
+                PathExclusions.AddRange(".Config,.config,.vbproj,.html,.htm,.css".Split(new char[] { ',' }));
+                //PostCompile.Add(  new RegexCompile(@"\[.* \d*\]", "")  );
+            }
+        }
         public class AppSettingsParser : BaseParser
         {
             public AppSettingsParser()
             {
                 //Path = @"c:\temp\save\*merge*.txt"; // AppSettings.DirCustomControls;
                 Path = @"C:\Users\Tim\source\repos\somuing\src\app\*.html";
-                ExcludeList.AddRange(AppSettings.ExcludeList.Split(new char[] { ',' }));
-                ExcludeList.AddRange("\\Triggers,\\Utils,\\InProgress".Split(new char[] { ',' }));
+                PathExclusions.AddRange(AppSettings.PathExclusions.Split(new char[] { ',' }));
+                PathExclusions.AddRange("\\Triggers,\\Utils,\\InProgress".Split(new char[] { ',' }));
        
             } 
         } 
